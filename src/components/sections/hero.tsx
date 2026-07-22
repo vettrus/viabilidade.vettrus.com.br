6ded064b0aaf0fbb691285817dd552bbd57a5276
Guilherme Cardoso <dev@vettrus.com.br>
2026-07-22 11:34:53 -0300

    fix: hero image

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -13,11 +13,11 @@ export function Hero() {
           fill
           priority
           sizes="100vw"
-          className="object-cover object-center opacity-75  "
+          className="object-cover object-center opacity-100"
         />
-        <div className="absolute inset-0 bg-gradient-to-b from-background/60 via-background/80 to-background" />
+        <div className="absolute inset-0 bg-gradient-to-b from-background/30 via-background/50 to-background" />
         {/* Sombra de cima para baixo */}
-        <div className="absolute inset-x-0 top-0 h-3/4 bg-gradient-to-b from-background via-background/70 to-transparent" />
+        <div className="absolute inset-x-0 top-0 h-3/4 bg-gradient-to-b from-background/80 via-background/40 to-transparent" />
       </div>
 
       <Container className="relative flex flex-col items-center pt-8 pb-12 text-center lg:pt-10 lg:pb-16">
